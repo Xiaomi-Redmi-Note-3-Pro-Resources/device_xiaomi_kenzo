d47671425dcb627aa9c069cbec03dab51441a5e2
TheStrix <parthbhatia98@gmail.com>
2016-11-08 14:06:49 +0530

    kenzo: Add tap to wake option
    [dodyirawan85]: also adapt to our kernel implementation
    
    Change-Id: Iec777ff7ef393ab1ef528f5f45ab3611eab4840b
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,5 +38,9 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
+# Tap-to-Wake
+TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
+
+
 # inherit from the proprietary version
 -include vendor/xiaomi/kenzo/BoardConfigVendor.mk
