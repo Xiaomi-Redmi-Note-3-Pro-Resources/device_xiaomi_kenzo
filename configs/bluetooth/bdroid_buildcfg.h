dea2f7a8f5a28f50d2a685d6d80317f32fe91bb5
TheScarastic <warabhishek@gmail.com>
2018-05-11 14:40:11 +0530

    kenzo: disable sco enahnced command
     * Adjust flag after https://github.com/LineageOS/android_system_bt/commit/540b42ceaab148e32a5881b7cdf0c7872ed14b13
    
    Change-Id: I8d2d47a7f39d5ec9f9a3d948c5b599ef7aae73da
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- configs/bluetooth/bdroid_buildcfg.h
+++ configs/bluetooth/bdroid_buildcfg.h
@@ -34,7 +34,7 @@
 #define BTM_LE_SECURE_CONN  TRUE
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
-#define BTM_SCO_ENHANCED_SYNC_DISABLED TRUE
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS 12
