4e21cca03d68ac3aa70fccf311b30311babb6c1c
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 17:11:10 +0200

    kenzo: Make Android.mk search recursive
    Change-Id: I3a05344f949eae581f1f17cc33b1df475d3e6760
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- Android.mk
+++ Android.mk
@@ -27,7 +27,8 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter kenzo,$(TARGET_DEVICE)),)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 include $(CLEAR_VARS)
 
