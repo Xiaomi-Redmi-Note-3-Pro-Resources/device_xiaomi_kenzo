c88a6b25f1939b1134f0a29edce49376f83bd1a8
LuK1337 <priv.luk@gmail.com>
2020-09-22 10:15:09 +0200

    kenzo: Enforce vintf manifest override
    * Apparently this is now required on all non full treble devices since
      google added a sleep(1) in libhidl...
    
    Change-Id: I6d8492dd7c56bfdb2ca00f3477634a46fa55b1c0
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -233,6 +233,8 @@ PRODUCT_PACKAGES += \
     charger
     
 # HIDL
+PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
+
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
