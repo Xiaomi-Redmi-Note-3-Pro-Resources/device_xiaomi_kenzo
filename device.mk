898a0496ba0090b70f1ae820ad5e4f2d2028abfd
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    kenzo: Switch to source built vendor.qti.hardware.perf@2.0.so
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    
    Change-Id: Ia5de8c300d71b6def223e97ef9d58898c9c11a57
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -367,7 +367,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats@1.0-service.mock \
+    vendor.qti.hardware.perf@2.0
 
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
