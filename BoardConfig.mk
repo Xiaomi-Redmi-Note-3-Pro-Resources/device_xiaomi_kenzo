63fe662164f4015a668dc646449aafbc3cd8e4fa
aranhid <vladmir909@mail.ru>
2021-04-15 19:09:05 +0300

    kenzo: sepolicy-minimal: fix build errors
    Change-Id: Idd293a7f103e51d1e6c808e271445da472b33089
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,9 +206,10 @@ USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR := true
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
 
 # SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+# include device/qcom/sepolicy-legacy/sepolicy.mk
+# BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+# SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
