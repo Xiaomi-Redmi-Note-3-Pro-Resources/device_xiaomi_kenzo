c683857ffd654361931440d107b2927b2a8fd029
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-14 16:26:14 +0200

    kenzo: Build vendor variant of android.hidl.base@1.0
    Change-Id: I4c01db7e8d37c8a033c6b0b5c504982de6a45ad3
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -262,6 +262,7 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0.vendor \
     libhidltransport \
