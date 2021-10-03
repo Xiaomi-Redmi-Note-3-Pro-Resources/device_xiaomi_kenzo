e15c80f257f05cf178bf5c6396440ede258c648c
Mocarafee <harif.rp05@gmail.com>
2021-10-03 11:56:56 +0700

    kenzo: Set kernel path to kenzo
    * Doesn't make sense to still using msm8956 as this trees is decommonized

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,7 +173,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8956
+TARGET_KERNEL_SOURCE := kernel/xiaomi/kenzo
 TARGET_KERNEL_CONFIG := lineageos_kenzo_defconfig
 
 # Keymaster
