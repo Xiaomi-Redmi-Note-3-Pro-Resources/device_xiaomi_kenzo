e7f4b06b33f7419cf3c4cb1cb192b16b864db6c7
Dominggoes Isakh <drjisakh@gmail.com>
2022-03-11 13:59:18 +0100

    kenzo: Set CLANG compile flag explicitly
    Sinse this commit https://github.com/LineageOS/android_vendor_lineage/commit/6d88852d2e602a92dde7bea849ff614f6a5165b1
    the flag TARGET_KERNEL_CLANG_COMPILE is set default to true.
    But our device still builds with the deprecated GCC. So set it
    explicit to false.
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/kenzo
+TARGET_KERNEL_CLANG_COMPILE := false
 TARGET_KERNEL_CONFIG := lineageos_kenzo_defconfig
 
 # Keymaster
