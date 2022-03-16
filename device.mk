08bcf69a5354df9bbb7da969f7f30b6bd6e62fb6
dianlujitao <dianlujitao@lineageos.org>
2022-03-16 17:05:56 +0800

    kenzo: Drop Snap
    Change-Id: Ie5e2c3d337a3d80795d6d307eb5cc4326d73bbd0
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -159,7 +159,6 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8952 \
     libqomx_core \
-    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
