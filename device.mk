23ed3174cc504f3d144861672c9f8db3a81a9962
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-13 20:19:49 -0400

    kenzo: Move to libhidl shim in hardware/lineage/compat
    Change-Id: Ia2cd305baf3ce0fa7ac52d440a4ff51f94654438
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -263,6 +263,7 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor \
     libhidltransport \
     libhwbinder \
     vndservicemanager
